43b5d479d6eba9d6be586c97eb194773e0250ecb
MH4GF <h.miyagi.cnw@gmail.com>
2022-05-15 21:38:11 +0900

    fix .eslintrc.json

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,13 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
-async function run(): Promise<void> {
+import {runTest} from './runTest'
+
+export const run = async (): Promise<void> => {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    await runTest()
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
 
-run()
+void run()
