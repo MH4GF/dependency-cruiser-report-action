32567a5f6cf5be205e53eca834bacb85f6a162ba
MH4GF <h.miyagi.cnw@gmail.com>
2022-05-16 00:23:59 +0900

    test for husky

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { runTest } from './runTest'
 export const run = async (): Promise<void> => {
   try {
     await runTest()
+    console.log('fuga')
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
