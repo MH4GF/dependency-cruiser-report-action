82cca2a20be84a3430b540334d6b81de1acbbc94
MH4GF <h.miyagi.cnw@gmail.com>
2022-05-15 21:42:57 +0900

    fix format

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,8 +1,8 @@
-import {wait} from '../src/wait'
+import { wait } from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
-import {expect, test} from '@jest/globals'
+import { expect, test } from '@jest/globals'
 
 test('throws invalid number', async () => {
   const input = parseInt('foo', 10)
@@ -23,7 +23,7 @@ test('test runs', () => {
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecFileSyncOptions = {
-    env: process.env
+    env: process.env,
   }
   console.log(cp.execFileSync(np, [ip], options).toString())
 })
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import * as core from '@actions/core'
 
-import {runTest} from './runTest'
+import { runTest } from './runTest'
 
 export const run = async (): Promise<void> => {
   try {
