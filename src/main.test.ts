7b3302950c1c70f13da040979c680c40ce21a852
MH4GF <h.miyagi.cnw@gmail.com>
2022-05-16 09:47:41 +0900

    fix test

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,7 +1,8 @@
-import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
-import { test } from '@jest/globals'
+import * as process from 'process'
+
+import { test } from 'vitest'
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test.skip('test runs', () => {
