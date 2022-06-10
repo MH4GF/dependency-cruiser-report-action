af4d879241baf34e03ea097f11f556ff97974fcb
Hirotaka Miyagi <31152321+MH4GF@users.noreply.github.com>
2022-06-11 00:32:17 +0900

    filter supported files (#73)
    * ignore unsupported files
    
    Closes https://github.com/MH4GF/dependency-cruiser-report-action/issues/52
    
    * fix test for runDepcruise
    
    * add comment

---
+++ src/options/filterSupportedFiles.ts
@@ -0,0 +1,27 @@
+// @see: https://github.com/sverweij/dependency-cruiser/blob/0b07cb71e059b5cea7856643d89fcea91b8df717/src/extract/transpile/meta.js#L14
+const SUPPORTED_EXTENSIONS = [
+  '.js',
+  '.cjs',
+  '.mjs',
+  '.jsx',
+  '.ts',
+  '.tsx',
+  '.d.ts',
+  '.ls',
+  '.coffee',
+  '.litcoffee',
+  '.coffee.md',
+  '.csx',
+  '.cjsx',
+  '.vue',
+  '.svelte',
+]
+
+export const filterSupportedFiles = (files: string[]) => {
+  const filtered = files.filter((file) => {
+    const ext = file.split('.').slice(-1)[0]
+    return SUPPORTED_EXTENSIONS.includes(`.${ext}`)
+  })
+
+  return filtered
+}
--- test/runDepcruise/index.test.ts
+++ test/runDepcruise/index.test.ts
@@ -4,21 +4,23 @@ import { fileURLToPath } from 'url'
 
 import { describe, it, expect } from 'vitest'
 
-import { runDepcruise } from '../../../src/runDepcruise'
+import { runDepcruise } from '../../src/runDepcruise'
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url))
-const mockPath = join(__dirname, '__mocks__')
 
 describe('runDepcruise', () => {
   it('execute depcruise command', async () => {
     const options = {
       targetFiles: `test/runDepcruise/sample/__mocks__/test`,
       focus: `"^test/runDepcruise/sample/__mocks__/test"`,
-      depcruiseConfigFilePath: `test/runDepcruise/sample/__mocks__/.dependency-cruiser.js`,
+      depcruiseConfigFilePath: `test/runDepcruise/.dependency-cruiser.js`,
       cruiseScript: 'yarn run -s depcruise',
     }
     const result = await runDepcruise(options)
-    const expectedMmd = readFileSync(join(mockPath, 'expected.mmd'), 'utf-8')
+    const expectedMmd = readFileSync(
+      join(__dirname, 'sample', '__mocks__', 'expected.mmd'),
+      'utf-8',
+    )
 
     expect(result.mermaidText).toStrictEqual(expectedMmd)
   })
