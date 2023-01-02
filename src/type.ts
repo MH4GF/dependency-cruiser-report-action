18e8270140f96b1dc3494a63b16981ba6f2f6be6
MH4GF <h.miyagi.cnw@gmail.com>
2023-01-02 16:47:16 +0900

    fix: remove orphan types

--- src/type.ts
+++ src/type.ts
@@ -1,6 +1,3 @@
 import { getOctokit } from '@actions/github'
-import { Context } from '@actions/github/lib/context'
 
-export type Repo = Context['repo']
-export type PullRequest = NonNullable<Context['payload']['pull_request']>
 export type Octokit = ReturnType<typeof getOctokit>
